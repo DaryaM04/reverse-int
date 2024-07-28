9f4e41d0b948e27741ec35b295e7a731409e2216
DaryaBoychenko <daryam963@yandex.ru>
2024-07-28 14:56:11 +0300

    fix: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if(n > 0){
+        return Array.from(n.toString())
+        .reverse()
+        .join('')
+    } else if (n < 0){
+        let result =  Array.from(n.toString())
+        .splice(1)
+        .reverse()
+        .join('')
+        return result;
+    }
+    
 }
